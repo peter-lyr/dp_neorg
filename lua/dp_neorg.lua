0ea337c9f44626bcfd3605b77e8a55f5b940fe19
peter-lyr <llydrp.ldp@gmail.com>
2024-06-29 11:05:03 +0800

    ^<s-cr^>

--- lua/dp_neorg.lua
+++ lua/dp_neorg.lua
@@ -428,10 +428,10 @@ require 'which-key'.register {
   ['<leader>nhe'] = { function() M.norg2md() end, 'norg2md', mode = { 'n', 'v', }, silent = true, },
   ['<leader>n<cr>'] = { function() M.create_norg_file_and_open() end, 'create_norg_file_and_open', mode = { 'n', 'v', }, silent = true, },
   ['<leader>n<c-cr>'] = { function() M.create_norg_file_and_open(1) end, 'create_norg_file_and_open journal', mode = { 'n', 'v', }, silent = true, },
-  ['<s-cr>'] = { function() M.create_norg_file_and_open(1) end, 'create_norg_file_and_open journal', mode = { 'n', 'v', }, silent = true, },
   ['<leader>n<del>'] = { function() M.de_norg_link() end, 'de_norg_link', mode = { 'n', 'v', }, silent = true, },
   ['<leader>n<tab>'] = { function() M.yank_rb_to_wxwork() end, 'yank_rb_to_wxwork', mode = { 'n', 'v', }, silent = true, },
-  ['<c-;>'] = { function() M.create_or_jump() end, 'create_or_jump', mode = { 'n', 'v', }, silent = true, },
+  -- ['<s-cr>'] = { function() M.create_norg_file_and_open(1) end, 'create_norg_file_and_open journal', mode = { 'n', 'v', }, silent = true, },
+  -- ['<c-;>'] = { function() M.create_or_jump() end, 'create_or_jump', mode = { 'n', 'v', }, silent = true, },
 }
 
 return M
